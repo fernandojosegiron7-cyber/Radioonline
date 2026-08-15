fd3e849e1f2234e4c5cedf40ee17d2baf251c706
fernandojosegiron7-cyber <fernandojosegiron7@gmail.com>
2026-08-15 00:47:46 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+window.RADIO_CONFIG = {
+  stationName: "ALÓ PAISANO RADIO",
+  tagline: "La música que nos conecta",
+  playlistUrl: "https://stream.zeno.fm/stm76db4tk8uv.m3u",
+  streamUrl: "https://stream.zeno.fm/stm76db4tk8uv",
+  zenoMount: "stm76db4tk8uv",
+  logoUrl: "/assets/logo.png",
+  defaultVolume: 80
+};
